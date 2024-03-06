f1a774950c9a1c6ce0789f879c0aa514038c821c
GMP Sankalpa <malithapiumal02@gmail.com>
2024-03-06 11:19:25 +0530

    new update

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("openBtn").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("openBtn").style.display = "block";
+}
